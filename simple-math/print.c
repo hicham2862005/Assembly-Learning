de68d3c549538871375787e934e7576788517ab0
hicham benyahia <hicham2862005hacking@gmail.com>
2026-03-30 21:08:32 +0100

    add more math operations

--- simple-math/print.c
+++ simple-math/print.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 
 int Add(int a, int b);
+int Sub(int a, int b);
+int Mul(int a, int b);
+int Div_q(int a, int b);
+int Div_r(int a, int b);
 
 int main()
 {
-	int result = Add(10,20);
+	int result = Div_q(10,20);
 	printf("%d\n",result);
 	return 0;
 }
