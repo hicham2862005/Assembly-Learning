2eefff152bde4a1b949067c6d0fa62c980725101
hicham benyahia <hicham2862005hacking@gmail.com>
2026-04-07 15:55:58 +0100

    add more FPU operations to the floating-point-math section

--- floating-point-math/print.c
+++ floating-point-math/print.c
@@ -4,10 +4,15 @@ float Add(float a, float b);
 float Sub(float a, float b);
 float Mul(float a, float b);
 float Div(float a, float b);
+// in those operations below "a" are considered to be in "radians"
+float Cos(float a);
+float Sin(float a);
+float Tan(float a);
+
 
 int main()
 {
-	float result = Div(10.0,20.0);
+	float result = Tan(30);
 	printf("%f\n",result);
 	return 0;
 }
