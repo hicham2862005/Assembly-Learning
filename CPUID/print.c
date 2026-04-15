0587f94ed4bc3e2a393f0220e893e365cd2e812c
hicham benyahia <hicham2862005hacking@gmail.com>
2026-04-15 14:59:15 +0100

    Add Basic DescribeFeature functionality to the CPUID section

--- CPUID/print.c
+++ CPUID/print.c
@@ -1,18 +1,47 @@
 #include<stdio.h>
+#include<stdint.h>	// for uint8_t and uint16_t type
 
-// TODO: Add other function for EAX ECX input
-// TODO: Add Enum for the FeatureIDs and some function for Outputing the output array in human readable way
+// this function are used to extract the CPUID Feature and display it in it's raw form
+void GetFeature(uint16_t FeatureID, uint32_t *output);
 
-void GetFeature1(int FeatureID, int *output);
+// this enum contians the FeatureID of each feature
+typedef enum {
+	BASIC_CPUID_INFORMATION_0 = 0,	// 00H
+	BASIC_CPUID_INFORMATION_1,	// 01H
+	BASIC_CPUID_INFORMATION_2,	// 02H
+	BASIC_CPUID_INFORMATION_3,	// 03H
+	DETERMINISTIC_CHACH_PARAMETERS,	// 04H
+	MONITOR_WAIT,			// 05H -- Monitor Wait (Mwait)
+} Features;
+
+// this function extracts the feature and Decode it into a human readable form
+void DescribeFeature(Features FeatureID)
+{
+	uint32_t output[4];		// output array for CPUID feature
+	GetFeature(FeatureID, output);
+	switch(FeatureID)
+	{
+		case BASIC_CPUID_INFORMATION_0:
+			printf("Maximum Input Value for CPUID:		%d\n",output[0]);
+			break;
+		case BASIC_CPUID_INFORMATION_1:
+			uint8_t SteppingID =	(output[0] & 0xf);
+			uint8_t Model = 	(output[0] & (0xf << 4));
+			uint8_t FamilyID =	(output[0] & (0xf << 8));
+			uint8_t ProcessorType =	(output[0] & (0x3 << 12));
+			printf("Stepping ID:				0x%02X\n",SteppingID);
+			printf("Model:					0x%02X\n",Model);
+			printf("Family ID:				0x%02X\n",FamilyID);
+			printf("Processor Type:				0x%02X\n",ProcessorType);
+			break;
+		default:
+			printf("Unsupported Feature ID\n");
+			break;
+	}
+}
 
 int main()
 {
-	int output[4];
-	GetFeature1(11, output);
-
-	printf("EAX: %d\n", output[0]);
-	printf("EBX: %d\n", output[1]);
-	printf("ECX: %d\n", output[2]);
-	printf("EDX: %d\n", output[3]);
+	DescribeFeature(BASIC_CPUID_INFORMATION_1);
 	return 0;
 }
